345dfcf7b9d6853c8c78a60881779f90d2b865f1
Luis Lopes <ll61295@gmail.com>
2022-12-14 10:16:07 +0000

    Add files via upload

---
+++ scripts/color.js
@@ -0,0 +1,40 @@
+addEventListener('click', go);
+
+const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
+
+async function changeColor() {
+    const onCall = document.querySelectorAll('button[label="On Call(Retail)"]')
+
+    for (var i = 0; i < onCall.length; i++) {
+        onCall[i].style.background = '#01baee';
+    }
+}
+
+async function go() {
+    await waitForCalendar();
+    console.log('checking if any on call')
+    await wait(500);
+    changeColor();
+}
+
+async function waitForCalendar() {
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(function () {
+            console.log('checking for calendar...');
+            const calendar = document.querySelector('.WMX3');
+            const nextMonthButton = document.querySelector('button[data-automation-id=nextMonthButton]');
+
+            if (calendar && nextMonthButton) {
+                const isActive = nextMonthButton.dataset.automationActivebutton === 'true';
+                if (isActive) {
+                    console.log('IT WORKED')
+                    clearInterval(interval);
+                    resolve();
+                }
+            }
+        }, 1000);
+    });
+}
+
+go();
+
